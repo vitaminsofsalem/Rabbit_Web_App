3fabf6c7a900280b869278912d5b1c56f2e0d515
Youssef Henna <youssef.hisham14@gmail.com>
2022-05-13 17:45:21 +0200

    Added starting projects for microservices

---
+++ services/notifications/src/modules/app/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { AppController } from "./app.controller";
+import { AppService } from "./services/app.service";
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: "KAFKA_CLIENT",
+        transport: Transport.KAFKA,
+        options: {
+          client: {
+            clientId: "notifications",
+            brokers: ["localhost:9092"],
+          },
+          consumer: {
+            groupId: "orders-consumer",
+          },
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
