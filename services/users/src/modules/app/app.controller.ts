66d160b2a7d2bcbb06bd7bfd36d458b48194c170
Youssef Henna <youssef.hisham14@gmail.com>
2022-05-15 15:05:42 +0200

    Added microservice for "user"

---
+++ services/users/src/modules/app/app.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Inject } from "@nestjs/common";
+import {
+  ClientKafka,
+  Ctx,
+  MessagePattern,
+  NatsContext,
+  Payload,
+} from "@nestjs/microservices";
+import { AppService } from "./services/app.service";
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    @Inject("KAFKA_CLIENT") private client: ClientKafka,
+  ) {
+    //Examples of emiting an event for the topic: messages
+    client.emit("messages", "some data");
+  }
+
+  //Example of handling an event on the topic: messages
+  //data can be any type, as long as same as what is being sent above
+  @MessagePattern("messages")
+  handleHelloMessage(@Payload("value") data: string) {
+    console.log("RECEIVED", data);
+  }
+}
