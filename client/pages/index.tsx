c0ea46d1880e8bbb96ad923ef85187c0f5787039
Youssef Henna <youssef.hisham14@gmail.com>
2022-05-15 15:23:02 +0200

    Added starting project for client

---
+++ client/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/index.module.css";
+
+const Home: NextPage = () => {
+  return <div>Rabbit</div>;
+};
+
+export default Home;
