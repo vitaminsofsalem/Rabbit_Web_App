8e3472fef0c02b25171d157537939d0b55af7c05
SohilaArafa <sohilaarafa2001@gmail.com>
2022-05-23 20:26:04 +0200

    dtos

--- services/orders/src/dtos/event.dto.ts
+++ services/orders/src/dtos/event.dto.ts
@@ -1,19 +1,9 @@
-import { IsDate, IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+import { IsDate, MaxLength,IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
 
 
 export class EventDto {
 
     @IsNotEmpty()
-    email: string;
-  
-    @IsNotEmpty()
-    address: string;
-    
-    @IsNotEmpty()
-    items: string[];
-  
-    @IsNotEmpty()
-    id: string ; 
 
     eventType: string;
   
