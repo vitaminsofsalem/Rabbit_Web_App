c6452863ea23341f6b849422ee89da59b2cfd2b2
SohilaArafa <sohilaarafa2001@gmail.com>
2022-05-23 20:32:32 +0200

    update dto

--- services/orders/src/dtos/create-service.dto.ts
+++ services/orders/src/dtos/create-service.dto.ts
@@ -3,16 +3,27 @@ import { EventDto } from './event.dto';
 import { PartialType } from '@nestjs/mapped-types';
 
 export class CreateServiceDto extends PartialType(EventDto) {
-    @IsNotEmpty()
-    email: string;
-  
-    @IsNotEmpty()
-    address: string;
-    
-    @IsNotEmpty()
-    items: string[];
-  
-    @IsNotEmpty()
-    id: string ; 
+   // @IsNotEmpty()
+   // email: string;
+  //
+   // @IsNotEmpty()
+   // address: string;
+   // 
+   // @IsNotEmpty()
+   // items: string[];
+  //
+   // @IsNotEmpty()
+   // id: string ; 
+    @MaxLength(64)
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @MaxLength(255)
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  tags: string[];
 
 }
