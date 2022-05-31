150fd5b0b1f896a60638fbe598710adba0185b7e
fakhry tatanaki <fakhry8821@icloud.com>
2022-05-31 23:03:17 +0200

    added loading animation

--- client/components/home_components/SpecialCategories.tsx
+++ client/components/home_components/SpecialCategories.tsx
@@ -1,4 +1,6 @@
 import styles from "../../styles/Home.module.scss";
+import { useEffect, useState } from "react";
+import dummyData from "../../dummyData/specialCategories.json";
 
 interface CategoryItem {
   title: String;
@@ -15,9 +17,28 @@ interface ArrowComponentProps {
   action?: () => void;
 }
 
-const SpecialCategoriesPager = () => (
-  <div className={styles.categoryPagerEntriesContainer}></div>
-);
+const SpecialCategoriesPager = (props: SpecialCategoriesProps) => {
+  if (props.items.length == 0) {
+    return (
+      <div className={styles.categoryPagerEntriesContainer}>
+        <div className={styles.itemLoading}></div>
+        <div className={styles.itemLoading}></div>
+        <div className={styles.itemLoading}></div>
+      </div>
+    );
+  }
+
+  return (
+    <div className={styles.categoryPagerEntriesContainer}>
+      {props.items.map((item) => (
+        <div className={styles.item}>
+          <p>{item.title}</p>
+          <img src={item.imageUrl as string} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const CategoryPagerButton = (props: ArrowComponentProps) => (
   <div className={styles.buttonContainer}>
@@ -33,12 +54,21 @@ const CategoryPagerButton = (props: ArrowComponentProps) => (
   </div>
 );
 
-const SpecialCategories = () => (
-  <div className={styles.specialCategories}>
-    <CategoryPagerButton direction="prev" />
-    <SpecialCategoriesPager />
-    <CategoryPagerButton direction="next" />
-  </div>
-);
+const SpecialCategories = () => {
+  const [items, setItems] = useState<CategoryItem[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setItems(dummyData as CategoryItem[]); //simulate loading process, for UI Testing only
+    }, 2000);
+  });
+  return (
+    <div className={styles.specialCategories}>
+      <CategoryPagerButton direction="prev" />
+      <SpecialCategoriesPager items={items} />
+      <CategoryPagerButton direction="next" />
+    </div>
+  );
+};
 
 export default SpecialCategories;
