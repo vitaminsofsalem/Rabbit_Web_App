d84abe4daaf17c25c1f30038c1ed65b94f501ca2
Youssef Henna <youssef.hisham14@gmail.com>
2022-06-01 20:34:14 +0200

    Added account page

--- client/pages/account/index.tsx
+++ client/pages/account/index.tsx
@@ -1,12 +1,38 @@
 import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import AccountTopItem from "../../components/account/AccountTopItem";
+import OrdersAddressButton from "../../components/account/OrdersAddressButton";
 import { PageWithNavBar } from "../../components/page_containers/PageWithNavBar";
+import styles from "../../styles/Account.module.scss";
 
 //URL: /account
 
 const AccountPage: NextPage = () => {
+  const router = useRouter();
   return (
     <PageWithNavBar>
-      <div></div>
+      <div className={styles.parentContainer}>
+        <AccountTopItem
+          name="Youssef Henna"
+          onEditClicked={() => {
+            router.push("/account/edit");
+          }}
+        />
+        <OrdersAddressButton
+          onClick={() => {
+            router.push("/account/orders");
+          }}
+          icon="orders"
+          label="My Orders"
+        />
+        <OrdersAddressButton
+          onClick={() => {
+            router.push("/account/address");
+          }}
+          icon="addresses"
+          label="My Addresses"
+        />
+      </div>
     </PageWithNavBar>
   );
 };
