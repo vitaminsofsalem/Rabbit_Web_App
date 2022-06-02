c4d864fbf0fdd0bc07858c5efacb3b412269ce7e
fakhry tatanaki <fakhry8821@icloud.com>
2022-06-02 20:55:30 +0200

    finished product card

--- client/components/home_components/CategoryProducts.tsx
+++ client/components/home_components/CategoryProducts.tsx
@@ -1,11 +1,50 @@
 import styles from "../../styles/Home.module.scss";
+
+interface ProductCardProps {
+  name: string;
+  priceEgp: number;
+  physicalDescription: string;
+  imageUrl?: string;
+}
+
+const ProductCard = (props: ProductCardProps) => {
+  const priceEgp = props.priceEgp.toFixed(2); //rounds to 2 decimal places
+  //
+  return (
+    <div className={styles.productCard}>
+      <div className={styles.image}></div>
+      <div className={styles.descriptions}>
+        <p className={styles.price}>
+          {priceEgp}
+          <div className={styles.currency}>EGP</div>
+        </p>
+        <p className={styles.name}>{props.name}</p>
+        <p className={styles.physicalDescription}>
+          {props.physicalDescription}
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const CategoryProducts = () => {
   return (
-    <div className={styles.categoryProductsContainer}>
+    <div className={styles.categoryProducts}>
       <div className={styles.categoryCard}>
         <h2>New Arrivals!</h2>
         <div className={styles.arrow}></div>
+        <div className={styles.categoryImage}></div>
       </div>
+      <ProductCard
+        name="Nutella Jar"
+        physicalDescription="500g"
+        priceEgp={70}
+      />
+      <ProductCard
+        name="Pril Diswahsing Soap Liquid Green for kitchen"
+        physicalDescription="220ml"
+        priceEgp={30}
+      />
     </div>
   );
 };
