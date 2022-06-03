b552375c17cc6c204a72473fea9db24ee399fc1f
SohilaArafa <sohilaarafa2001@gmail.com>
2022-06-03 17:32:14 +0200

    more Dtos

--- services/orders/src/dtos/UserOrdersDto.ts
+++ services/orders/src/dtos/UserOrdersDto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString} from "class-validator";
 
-export default class GetUserOrdersDto  {
+export default class UserOrdersDto  {
 
   email : string;
   type: "GET_ORDERS_REQUEST";
--- services/orders/src/dtos/UserOrdersRespDto.ts
+++ services/orders/src/dtos/UserOrdersRespDto.ts
@@ -1,6 +1,6 @@
 import { IsNotEmpty, IsString} from "class-validator";
 
-export default class GettingOrderDto  {
+export default class UserOrdersRespDto  {
 
   email : string;
   type: "GET_ORDERS_RESPONSE";
