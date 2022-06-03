48e25115fb78afdb3654aac907343ae405f10018
fakhry tatanaki <fakhry8821@icloud.com>
2022-06-03 02:38:06 +0200

    some adjustments

--- client/components/home_components/CategoryProducts.tsx
+++ client/components/home_components/CategoryProducts.tsx
@@ -1,28 +1,31 @@
 import styles from "../../styles/Home.module.scss";
 import ProductCard from "../ProductCard";
 
-const CategoryProducts = () => {
+interface CategoryProductsProps {
+  title: string;
+  color: string;
+  products?: any; //kept 'any' until we agree on a common product objec structure
+}
+const CategoryProducts = (props: CategoryProductsProps) => {
   return (
     <div className={styles.categoryProducts}>
-      <div className={styles.categoryCard}>
-        <h2>New Arrivals!</h2>
+      <div
+        className={styles.categoryCard}
+        style={{ backgroundColor: props.color }}
+      >
+        <h2>{props.title}</h2>
         <div className={styles.arrow}></div>
         <div className={styles.categoryImage}></div>
       </div>
-      <ProductCard
-        name="Nutella Jar"
-        physicalDescription="500g"
-        priceEgp={70}
-        maxQuantity={6}
-        onQuantityChange={(q) => q} //does nothing for now
-      />
-      <ProductCard
-        name="Pril Diswahsing Soap Liquid Green for kitchen"
-        physicalDescription="220ml"
-        priceEgp={30}
-        maxQuantity={3}
-        onQuantityChange={(q) => q}
-      />
+      {props.products.map((p: any) => (
+        <ProductCard
+          name={p.name}
+          physicalDescription={p.physicalDescription}
+          priceEgp={p.priceEgp}
+          maxQuantity={p.maxQuantity}
+          onQuantityChange={(q) => q} //does nothing for now
+        />
+      ))}
     </div>
   );
 };
--- client/pages/home/index.tsx
+++ client/pages/home/index.tsx
@@ -6,12 +6,31 @@ import styles from "../../styles/Home.module.scss";
 
 //URL: /home
 
+const mockProducts: any[] = [
+  {
+    name: "Nutella Jar",
+    physicalDescription: "500g",
+    priceEgp: 70,
+    maxQuantity: 3,
+  },
+  {
+    name: "Pril Diswahsing Soap Liquid Green for kitchen",
+    physicalDescription: "220ml",
+    priceEgp: 30,
+    maxQuantity: 3,
+  },
+];
+
 const HomePage: NextPage = () => {
   return (
     <PageWithNavBar>
       <div className={styles.homeContainer}>
         <SpecialCategories />
-        <CategoryProducts />
+        <CategoryProducts
+          title="new arrivals!"
+          color="#208F5C"
+          products={mockProducts}
+        />
       </div>
     </PageWithNavBar>
   );
