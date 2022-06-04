b6db50602bacb6391c26e8862f163e11b2974f44
SohilaArafa <sohilaarafa2001@gmail.com>
2022-06-04 18:59:56 +0200

    unwanted import

--- services/orders/src/schemas/order.schema.ts
+++ services/orders/src/schemas/order.schema.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { Document, Mongoose } from "mongoose";
+import { Document} from "mongoose";
 
 export type OrdersDocument = Orders & Document;
 
