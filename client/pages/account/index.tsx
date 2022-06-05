673b1f6081c7c3ff9e6be2d0a47796b53ed0b75e
Youssef Henna <youssef.hisham14@gmail.com>
2022-06-05 12:12:20 +0200

    Client: added log out button

--- client/pages/account/index.tsx
+++ client/pages/account/index.tsx
@@ -1,14 +1,17 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
+import { useContext } from "react";
 import AccountTopItem from "../../components/account/AccountTopItem";
 import OrdersAddressButton from "../../components/account/OrdersAddressButton";
 import { PageWithNavBar } from "../../components/page_containers/PageWithNavBar";
+import { GlobalStateContext } from "../../model/GlobalState";
 import styles from "../../styles/Account.module.scss";
 
 //URL: /account
 
 const AccountPage: NextPage = () => {
   const router = useRouter();
+  const [globalState, setGlobalState] = useContext(GlobalStateContext);
   return (
     <PageWithNavBar>
       <div className={styles.parentContainer}>
@@ -32,6 +35,26 @@ const AccountPage: NextPage = () => {
           icon="addresses"
           label="My Addresses"
         />
+        <div
+          onClick={() => {
+            localStorage.removeItem("token");
+            setGlobalState({
+              ...globalState,
+              isLoggedIn: false,
+              loggedInUserName: undefined,
+              addresses: [],
+              cart: [],
+              selectedAddress: undefined,
+            });
+          }}
+          className={`${styles.ordersAddressButtonContainer} ${styles.signOutButtonContainer}`}
+        >
+          <p
+            className={`${styles.ordersAddressButtonText} ${styles.signOutText}`}
+          >
+            Log out
+          </p>
+        </div>
       </div>
     </PageWithNavBar>
   );
