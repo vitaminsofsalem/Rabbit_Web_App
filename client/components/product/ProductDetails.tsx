1b14d242488df0031c090c89e71b394de8345075
Youssef Henna <youssef.hisham14@gmail.com>
2022-06-07 21:54:58 +0200

    Client: Integrated product details with api

--- client/components/product/ProductDetails.tsx
+++ client/components/product/ProductDetails.tsx
@@ -1,26 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
 import { Product } from "../../model/Product";
+import { getProductDetails } from "../../remote/product";
 import styles from "../../styles/Product.module.scss";
 import { useCartUpdater } from "../../util/CartUpdaterHook";
 import ExternalImage from "../common/ExternalImage";
 import QuantiyItem from "./QuantityItem";
-
-//TODO: Remove mock and integrate with api
-const product: Product = {
-  id: "1",
-  name: "Nutella Jar",
-  subtext: "500g",
-  price: 70,
-  imageUrl:
-    "http://fhtrpi4.ddns.net:8821/e4f2f072b3821e7e004d80d6e469962d09c4c01a_356882_01.png",
-  currentQuantity: 3,
-};
 
 interface ProductDetailsPageProps {
   id: string;
 }
 
 const ProductDetails: React.FC<ProductDetailsPageProps> = ({ id }) => {
+  const [product, setProduct] = useState<Product | undefined>(undefined);
+
   const {
     incrementQuantity,
     decrementQuantity,
@@ -28,51 +21,66 @@ const ProductDetails: React.FC<ProductDetailsPageProps> = ({ id }) => {
     currentCart,
   } = useCartUpdater();
 
+  useEffect(() => {
+    toast
+      .promise(getProductDetails(id), {
+        pending: "Getting product",
+        error: "Failed to get product",
+      })
+      .then((productResult) => {
+        setProduct(productResult.product);
+      });
+  }, []);
+
   const [isFavorite, setIsFavorite] = useState(false);
 
   return (
-    <div className={styles.productDetailsMainContainer}>
-      <ExternalImage
-        width={254}
-        height={254}
-        className={styles.image}
-        src={product.imageUrl}
-        objectFit="contain"
-      />
-      <div className={styles.productDetailsBoxContainer}>
-        <p className={styles.productDetailsName}>{product.name}</p>
-        <p className={styles.productDetailsPhysicalDescription}>
-          {product.subtext}
-        </p>
-        <div className={styles.productDetailsDivider} />
-        <div className={styles.productDetailsBottom}>
-          <div
-            onClick={() => setIsFavorite(!isFavorite)}
-            className={`${styles.productDetailsHeart} ${
-              isFavorite ? styles.productDetailsHeartSelected : ""
-            }`}
+    <>
+      {product && (
+        <div className={styles.productDetailsMainContainer}>
+          <ExternalImage
+            width={254}
+            height={254}
+            className={styles.image}
+            src={product.imageUrl}
+            objectFit="contain"
           />
-          <div className={styles.productDetailsDivider} />
-          <p className={styles.productDetailsPrice}>
-            {product.price}
-            <span className={styles.currency}>EGP</span>
-          </p>
-          {product.currentQuantity > 0 ? (
-            <QuantiyItem
-              additionalClassName={styles.addButton}
-              onDecrement={() => decrementQuantity(product)}
-              onIncrement={() => incrementQuantity(product)}
-              quantity={getCurrentQuantity(product)}
-              quantityInStock={product.currentQuantity}
-            />
-          ) : (
-            <div className={styles.outOfStockMsg}>
-              <p>Out Of Stock</p>
+          <div className={styles.productDetailsBoxContainer}>
+            <p className={styles.productDetailsName}>{product.name}</p>
+            <p className={styles.productDetailsPhysicalDescription}>
+              {product.subtext}
+            </p>
+            <div className={styles.productDetailsDivider} />
+            <div className={styles.productDetailsBottom}>
+              <div
+                onClick={() => setIsFavorite(!isFavorite)}
+                className={`${styles.productDetailsHeart} ${
+                  isFavorite ? styles.productDetailsHeartSelected : ""
+                }`}
+              />
+              <div className={styles.productDetailsDivider} />
+              <p className={styles.productDetailsPrice}>
+                {product.price}
+                <span className={styles.currency}>EGP</span>
+              </p>
+              {product.currentQuantity > 0 ? (
+                <QuantiyItem
+                  additionalClassName={styles.addButton}
+                  onDecrement={() => decrementQuantity(product)}
+                  onIncrement={() => incrementQuantity(product)}
+                  quantity={getCurrentQuantity(product)}
+                  quantityInStock={product.currentQuantity}
+                />
+              ) : (
+                <div className={styles.outOfStockMsg}>
+                  <p>Out Of Stock</p>
+                </div>
+              )}
             </div>
-          )}
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
