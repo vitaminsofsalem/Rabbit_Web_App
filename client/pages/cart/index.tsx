d7092c73c8add79c5f580366e4117c55f7e37d60
vitaminsofsalem <jokydokyduck@gmail.com>
2022-06-09 21:41:56 +0200

    implemented empty cart page

--- client/pages/cart/index.tsx
+++ client/pages/cart/index.tsx
@@ -1,10 +1,16 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { PageWithNavBar } from "../../components/page_containers/PageWithNavBar";
 import { useCartUpdater } from "../../util/CartUpdaterHook";
+import styles from "../../styles/Cart.module.scss";
+import Button from "../../components/common/Button";
+import { useRouter } from "next/router";
 
 //URL: /cart
 
 const CartPage: NextPage = () => {
+  const router = useRouter();
+
   //Use these functions for any changes to cart items
   const {
     incrementQuantity,
@@ -15,7 +21,22 @@ const CartPage: NextPage = () => {
 
   return (
     <PageWithNavBar>
-      <div></div>
+      <div className={styles.parent}>
+        <div className={styles.container}>
+          <Image
+            height={175}
+            width={175}
+            src={require("/../client/assets/Icons/cart.svg")}
+            alt="Shopping_Cart_Logo"
+          />
+          <text className={styles.text}>It seems your cart is still empty</text>
+          <div className={styles.button}>
+            <Button onClick={() => router.push("/home")}>
+              {"Browse Products"}
+            </Button>
+          </div>
+        </div>
+      </div>
     </PageWithNavBar>
   );
 };
