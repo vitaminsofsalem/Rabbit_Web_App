a696e12d6de7c91d8b48a34b1993f98b140746f3
fakhry tatanaki <fakhry8821@icloud.com>
2022-06-10 16:13:56 +0200

    implemented basic payment for orders

--- services/payments/src/modules/app/app.controller.ts
+++ services/payments/src/modules/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Inject } from "@nestjs/common";
+import { Controller, Get, Inject } from "@nestjs/common";
 import {
   ClientKafka,
   Ctx,
@@ -19,8 +19,56 @@ export class AppController {
 
   //Example of handling an event on the topic: messages
   //data can be any type, as long as same as what is being sent above
-  // @MessagePattern("payments")
-  // handleHelloMessage(@Payload("value") data: string) {
-  //   console.log("RECEIVED", data);
-  // }
+
+  @MessagePattern("payments")
+  async onPayment(
+    @Payload("value")
+    data: {
+      orderTotal: number;
+      orderId: string;
+      requestId: string;
+      type: string;
+    },
+  ) {
+    switch (data.type) {
+      case "CREATE_PAYMENT_REQUEST":
+        try {
+          const session = await this.stripe.checkout.sessions.create({
+            line_items: [
+              {
+                price_data: {
+                  currency: "egp",
+                  product_data: {
+                    name: data.orderId,
+                  },
+                  unit_amount: data.orderTotal,
+                },
+                quantity: 1,
+              },
+            ],
+            mode: "payment",
+            success_url: "https://example.com/success",
+            cancel_url: "https://example.com/cancel",
+          });
+
+          this.client.emit("payments", {
+            ...data,
+            type: "PENDING_PAYMENT",
+            paymentUrl: session.url,
+          });
+        } catch (err) {
+          console.log(err);
+          this.client.emit("payments", {
+            ...data,
+            type: "PAYMENT_ERROR",
+          });
+        }
+
+      // const paymentIntent = await this.stripe.paymentIntents.create({
+      //   amount: data.amount,
+      //   currency: "egp",
+      //   payment_method_types: ["card"],
+      // });
+    }
+  }
 }
